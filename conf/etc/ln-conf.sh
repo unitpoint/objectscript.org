ef2a6d0291009b4873ffc9c6da810f556d3bb03b
Evgeniy Golovin <evgeniy.golovin@unitpoint.ru>
2013-12-09 16:19:34 +0400

    fix typo

--- conf/etc/ln-conf.sh
+++ conf/etc/ln-conf.sh
@@ -4,7 +4,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 ln -s /home/os/objectscript.org/conf/etc/nginx/nginx.conf /etc/nginx/nginx.conf
 ln -s /home/os/objectscript.org/conf/etc/nginx/fastcgi_params /etc/nginx/fastcgi_params
 ln -s /home/os/objectscript.org/conf/etc/nginx/ssl.conf /etc/nginx/ssl.conf
-ln -s /home/os/objectscript.org/conf/etc/nginx/sites-available/objectscript.org/etc/nginx/sites-available/objectscript.org
+ln -s /home/os/objectscript.org/conf/etc/nginx/sites-available/objectscript.org /etc/nginx/sites-available/objectscript.org
 # ln -s /etc/nginx/sites-available/objectscript.org /etc/nginx/sites-enabled/objectscript.org
 
 
